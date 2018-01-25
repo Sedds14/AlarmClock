e2a98031dad4183f5e85744bca5ff423d022921e
Sedds14 <caseddon@live.co.uk>
2018-01-25 10:47:00 +0000

    More tidying and inserted docstrings

--- clock.py
+++ clock.py
@@ -1,4 +1,24 @@
-#!/usr/bin/python
+"""""
+clock.py
+
+To be deployed on a WiFi enabled RPi as a digital alarm clock.
+Peripherals
+    - Adafruit 7 Segment Display with backpack
+    - Button - on pin 12
+    - Connection to Philips Hue Bridge through WiFi network
+
+Current functionality
+    - Tells the time!
+    - Dark when hue light in my room is off
+    - Low light setting shows time on button press
+
+To Do:
+    - Alarm: Needs a speaker hacking onto it and a connection to google
+      calender
+    - Battery backup?
+    - Mic, privacy switch and button for alexa?
+    - A case!
+"""""
 
 import time
 import datetime
@@ -11,6 +31,9 @@ from phue import Light
 
 
 def main():
+    """
+    Sets up periferals and then begins loop
+    """
 
     # Setup Display
     segment = SevenSegment.SevenSegment(address=0x70)
@@ -28,6 +51,16 @@ def main():
     # Initialise the bedroom light
     light = Light(bridge, 'Bedroom')
 
+    loop()
+
+
+def loop():
+    """
+    This is the main loop, it loops every 0.25 secs
+    and calls other functions to update the display
+    according to the user inputs
+    """
+
     while(True):
         # Wait a quarter second (less than 1 second to prevent colon
         # blinking getting annoying)  
@@ -35,7 +68,7 @@ def main():
         if light.on:
             clockUpdate(4)
         # If the room light is on update time at normal
-        # brightness (normal operation
+        # brightness (normal operation)
         elif GPIO.input(BtnPin) is 0:
             clockUpdate(0)
         # Else if the button is pressed show the
@@ -45,7 +78,15 @@ def main():
         # Else do not show the time. It's sleepy time.
 
 
-def clockUpdate(brightness):
+def clock_update(brightness):
+    """
+    Updates the clock with the current time.
+    Arguments:
+        brightness - INT from 0-14 setting LED bridgtness
+    Returns:
+        NULL
+    """
+
     # clear the buffer
     segment.clear()
     # set display brightness
@@ -66,8 +107,12 @@ def clockUpdate(brightness):
     # Write the display buffer to the hardware.
     segment.write_display()	
 
-  
-def clockOff():
+
+def clock_off():
+    """
+    This function turns off the display by printing an empty string and turning
+    off the colon.
+    """
     # clear the buffer
     segment.clear()
     # The light is off so the room should be dark to achieve this effect...
